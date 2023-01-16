594a53f6b06a2f1b6c62657c3c7c90b6ea7b5ccc
hhasanvrcreator <hammadvrcreator@gmail.com>
2023-01-16 22:31:08 +0500

    done

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const summryField = document.querySelector('.summry');
 const debitField = document.querySelector('#debitTotal');
 const creditField = document.querySelector('#creditTotal');
 
-document.querySelector('#username').textContent = getlocal('user_name');
-
 const accounts = JSON.parse(getlocal('accounts'));
 const user_id = getlocal('user_id');
+
+document.querySelector('#username').textContent = getlocal('user_name');
 
 function checkLogin() {
     if (localStorage.getItem('token') == null) {
@@ -34,22 +34,53 @@ function getCurrentUser() {
     return accounts.find(({ id }) => id.toString() === user_id.toString());
 }
 
-function fetchMovements() {
+
+function fetchMovements(sorted = false) {
     const user_account = getCurrentUser();
     balanceField.innerHTML = user_account.balance;
     let movementsHtml = '';
     if (user_account.movements.length > 0) {
-        user_account.movements.slice().reverse().forEach(element => {
-            movementsHtml += `<p>${element.includes('-') ? 'Debit : ' : 'Credit : '}${element}</p>`
-        });
-        // summryField.classList.replace('hide','show');
-        summryField.style.display = "block";
-        getSummry();
+        if (sorted) {
+            user_account.movements.slice().forEach(element => {
+                let amountDate = new Date(element.date);
+                movementsHtml += `<p>${element.amount.includes('-') ? 'Debit : ' : 'Credit : '}${element.amount}
+                <span>${formatMovementDate(amountDate)}</span>
+                </p>`
+            });
+        } else {
+            user_account.movements.slice().reverse().forEach(element => {
+                let amountDate = new Date(element.date);
+                movementsHtml += `<p>${element.amount.includes('-') ? 'Debit : ' : 'Credit : '}${element.amount}
+                <span>${formatMovementDate(amountDate)}</span>
+                </p>`
+            });
+        }
     } else {
         movementsHtml = '<p>No Transaction</p>';
     }
     movementsField.innerHTML = movementsHtml;
+    summryField.style.display = "block";
+    getSummry();
 }
+
+
+const formatMovementDate = function (date) {
+    console.log(date);
+    const calcDaysPassed = (date1, date2) =>
+      Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+    if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return "Yesterday";
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+  
+    // const day = `${date.getDate()}`.padStart(2, 0);
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    // const year = date.getFullYear();
+    // return `${day}/${month}/${year}`;
+    // return new Intl.DateTimeFormat(locale).format(date);
+  };
 
 function fetchUsers() {
     let usersHtml = '';
@@ -64,7 +95,7 @@ function fetchUsers() {
 
 
 function sendMoney() {
-    if(sendingAmountField.value == "" || sendingAmountField.value == null){
+    if (sendingAmountField.value == "" || sendingAmountField.value == null) {
         alert('Please Enter Amount');
         return false;
     }
@@ -78,30 +109,44 @@ function sendMoney() {
     let receiver_data_index = accounts.findIndex(element => element.id == receiver_id);
     let receiver_data = accounts.filter(element => element.id == receiver_id);
     receiver_data[0].balance += amount;
-    receiver_data[0].movements.push(`+${amount}`);
+    let movement = {"amount" : `+${amount}`, "date" : new Date().toISOString()};
+    receiver_data[0].movements.push(movement);
+    // receiver_data[0].movements.push(`+${amount}`);
     let sender_data_index = accounts.findIndex(element => element.id == user_id);
     let sender_data = accounts.filter(element => element.id == user_id);
     sender_data[0].balance -= amount;
-    sender_data[0].movements.push(`-${amount}`);
+    movement = {"amount" : `-${amount}`, "date" : new Date().toISOString()};
+    sender_data[0].movements.push(movement);
+    // sender_data[0].movements.push(`-${amount}`);
     accounts[receiver_data_index] = receiver_data[0];
     accounts[sender_data_index] = sender_data[0];
     setlocal('accounts', JSON.stringify(accounts));
     sendingAmountField.value = "";
     fetchMovements();
+    // summryField.classList.replace('hide','show');
+    summryField.style.display = "block";
+    getSummry();
 }
 document.querySelector('#sendButton').addEventListener('click', sendMoney);
 
 function getSummry() {
     const user_account = getCurrentUser();
     const moves = user_account.movements;
-    const debitSummry = moves.filter(element => element.includes('-')).reduce((acc, value) => acc + parseInt(value), 0)
-    const creditSummry = moves.filter(element => element.includes('+')).reduce((acc, value) => acc + parseInt(value), 0)
-    console.log('summries');
-    console.log(debitSummry)
-    console.log(creditSummry)
+    const debitSummry = moves.filter(element => element.amount.includes('-')).reduce((acc, value) => acc + parseInt(value.amount), 0)
+    const creditSummry = moves.filter(element => element.amount.includes('+')).reduce((acc, value) => acc + parseInt(value.amount), 0)
+    // console.log('summries');
+    // console.log(debitSummry)
+    // console.log(creditSummry)
     debitField.textContent = Math.abs(debitSummry);
     creditField.textContent = Math.abs(creditSummry);
 }
+
+let sorted = false;
+//sort button click
+document.querySelector('#sortButton').addEventListener('click', function () {
+    fetchMovements(!sorted);
+    sorted = !sorted;
+});
 
 
 // logout
